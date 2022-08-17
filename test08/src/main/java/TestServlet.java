e0ebbdd39b66bd464efc8570d38b55884002e2ad
dongukk <qkdnvd@hanmail.net>
2022-08-17 17:51:04 +0900

    commit

--- test08/src/main/java/TestServlet.java
+++ test08/src/main/java/TestServlet.java
@@ -21,13 +21,9 @@ public class TestServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Testservlet");
+	
 	}
 
 	/**
