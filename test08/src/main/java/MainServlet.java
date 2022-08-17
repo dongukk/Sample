aafc580b67fe62ac84cb26dbb73e043bbdcbe9da
dongukk <qkdnvd@hanmail.net>
2022-08-17 18:08:23 +0900

    aa

--- test08/src/main/java/MainServlet.java
+++ test08/src/main/java/MainServlet.java
@@ -27,6 +27,7 @@ public class MainServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("MainServ");
+		System.out.println("conflict");
 		System.out.println("Testservlet");
 	}
 
