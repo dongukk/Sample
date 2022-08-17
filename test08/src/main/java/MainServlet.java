c3d20b8712cdbb45669753db121f0bb2afc0a1ec
dongukk <qkdnvd@hanmail.net>
2022-08-17 18:06:00 +0900

    commit 수정

--- test08/src/main/java/MainServlet.java
+++ test08/src/main/java/MainServlet.java
@@ -26,7 +26,7 @@ public class MainServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("MainServlet");
+		System.out.println("MainServ");
 		System.out.println("Testservlet");
 	}
 
